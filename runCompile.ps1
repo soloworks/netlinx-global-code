8aa8f9f2a49957e997fe52b48b1be677f35f64ab
Sam Shelton (Office Desktop) <sam.shelton@soloworks.co.uk>
2019-05-28 19:25:57 +0100

    Added Passthrough

--- runCompile.ps1
+++ runCompile.ps1
@@ -5,7 +5,7 @@
 $ROOT = (Get-Item -Path ".\").FullName
 
 # Compile the Workspace from the Compiler .cfg file
-$P = Start-Process -FilePath 'C:\Program Files (x86)\Common Files\AMXShare\COM\NLRC.exe' -ArgumentList  "-CFG""$($ROOT)\compile.cfg""" -NoNewWindow -Wait
+$P = Start-Process -FilePath 'C:\Program Files (x86)\Common Files\AMXShare\COM\NLRC.exe' -ArgumentList  "-CFG""$($ROOT)\compile.cfg""" -NoNewWindow -Wait -PassThru
 
 # Exit wtith Compiler Exit Code
 exit $P.ExitCode
