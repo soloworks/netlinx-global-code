8323c5bb3ee03a09407ce6d24a3c097b329271ba
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-07-13 11:48:54 +0100

    Consolidated Piepling into single file

--- getReport.ps1
+++
@@ -1,9 +0,0 @@
-
-$ROOT = (Get-Item -Path ".\").FullName
-$URI = "https://us-central1-soloworkslondon.cloudfunctions.net/ProcessNetlinxCompileLog?"
-$URI = "$($URI)root=$($ROOT)"
-
-$R = Invoke-WebRequest -Uri $URI -Method POST -InFile ".\compile.log" -OutFile ".\clean.log" -PassThru -UseBasicParsing
-
-# Output to Console
-Get-Content -Path ".\clean.log"
